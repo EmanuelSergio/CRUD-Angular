a32b676cc7b5090a180bfeff49821f1ad869f84f
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2023-10-17 23:39:47 -0300

    construção da home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,15 +10,19 @@ import {MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 
+import { MatCardModule } from '@angular/material/card'
 import { MatSidenavModule } from '@angular/material/sidenav'
-import {MatListModule} from '@angular/material/list'
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    HeaderComponent, 
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent, 
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,8 @@ import {MatListModule} from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule, 
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
